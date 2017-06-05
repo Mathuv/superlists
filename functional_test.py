acfcc1670c11ecdbf8e272bc8f3bf579c9234a81
Mathu Mounasamy <mmounasamy@gmail.com>
2017-06-05 21:12:29 +1000

    Cleans up functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 
-print("Hello Mathu")
-
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-print("Hello Mathu")
 assert 'Django' in browser.title 
-assert 'Mathu' in browser.title 
